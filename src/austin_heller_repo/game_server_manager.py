1ca81b3dc44dd7d6cb560d079137a1c50aa38b57
austin <galaky2005@gmail.com>
2021-12-04 21:44:34 -0500

    moved StringEnum to Common library; GameServerManagerMessage is now expected from the socket and can be sent/parsed; GameServerManagerServer now only requires process_message to be implemented;

--- src/austin_heller_repo/game_server_manager.py
+++ src/austin_heller_repo/game_server_manager.py
@@ -1,20 +1,10 @@
 from __future__ import annotations
+from austin_heller_repo.common import StringEnum
+from austin_heller_repo.socket import ServerSocketFactory, ServerSocket, ClientSocket
+from austin_heller_repo.threading import Semaphore
 from abc import ABC, abstractmethod
 from typing import List, Tuple, Dict
-
-
-class StringEnum():
-
-	@classmethod
-	def get_list(cls) -> List[str]:
-		class_attribute_names = dir(cls)
-		list_of_string_attributes = []  # type: List[str]
-		for class_attribute_name in class_attribute_names:
-			if not class_attribute_name.startswith("__"):
-				class_attribute_value = getattr(cls, class_attribute_name)
-				if isinstance(class_attribute_value, str):
-					list_of_string_attributes.append(class_attribute_value)
-		return list_of_string_attributes
+import json
 
 
 class GameServerManagerMessageTypeEnum(StringEnum):
@@ -29,16 +19,34 @@ class GameServerManagerMessageTypeEnum(StringEnum):
 
 class GameServerManagerMessage():
 
-	def __init__(self, *, game_server_manager_message_type: str, message_json: Dict):
+	def __init__(self, *, game_server_manager_message_type: GameServerManagerMessageTypeEnum, message_json: Dict):
 
 		self.__game_server_manager_message_type = game_server_manager_message_type
 		self.__message_json = message_json
 
-	def get_game_server_manager_message_type(self) -> str:
+	def get_game_server_manager_message_type(self) -> GameServerManagerMessageTypeEnum:
 		return self.__game_server_manager_message_type
 
 	def get_message_json(self) -> Dict:
 		return self.__message_json
+
+	def to_json(self) -> Dict:
+		return {
+			"game_server_manager_message_type": self.__game_server_manager_message_type.value,
+			"message_json": self.__message_json
+		}
+
+	@staticmethod
+	def parse_from_json(*, json_string: str) -> GameServerManagerMessage:
+		json_object = json.loads(json_string)
+		game_server_manager_message_type_value = json_object["game_server_manager_message_type"]
+		message_json = json_object["message_json"]
+		game_server_manager_message_type = GameServerManagerMessageTypeEnum(game_server_manager_message_type_value)
+		game_server_manager_message = GameServerManagerMessage(
+			game_server_manager_message_type=game_server_manager_message_type,
+			message_json=message_json
+		)
+		return game_server_manager_message
 
 
 class GameServerManagerClient():
@@ -57,6 +65,8 @@ class GameServerManagerClient():
 		raise NotImplementedError()
 
 	def read_message(self) -> GameServerManagerMessage:
+
+		# TODO read from ClientSocket and parse message
 
 		raise NotImplementedError()
 
@@ -78,19 +88,53 @@ class GameServerManagerClientFactory():
 
 class GameServerManagerServer(ABC):
 
-	def __init__(self, *, host_address: str, host_port: int):
+	def __init__(self, *, server_socket_factory: ServerSocketFactory, host_address: str, host_port: int):
 
+		self.__server_socket_factory = server_socket_factory
 		self.__host_address = host_address
 		self.__host_port = host_port
 
-	@abstractmethod
+		self.__server_socket = None  # type: ServerSocket
+		self.__client_sockets = []  # type: List[ClientSocket]
+		self.__client_sockets_semaphore = Semaphore()
+		self.__is_client_sockets_active = True
+
+	def __on_accepted_client_method(self, client_socket: ClientSocket):
+
+		self.__client_sockets_semaphore.acquire()
+		self.__client_sockets.append(client_socket)
+		self.__client_sockets_semaphore.release()
+
+		while self.__is_client_sockets_active:
+			game_server_manager_message_json_string = client_socket.read()
+			game_server_manager_message = GameServerManagerMessage.parse_from_json(
+				json_string=game_server_manager_message_json_string
+			)
+			try:
+				self.process_message(
+					game_server_manager_message=game_server_manager_message
+				)
+			except Exception as ex:
+				print(f"__on_accepted_client_method: process_message: ex: {ex}")
+
 	def start(self):
-		raise NotImplementedError()
+
+		self.__server_socket = self.__server_socket_factory.get_server_socket()
+		self.__server_socket.start_accepting_clients(
+			host_ip_address=self.__host_address,
+			host_port=self.__host_port,
+			on_accepted_client_method=self.__on_accepted_client_method
+		)
 
 	@abstractmethod
 	def process_message(self, *, game_server_manager_message: GameServerManagerMessage):
 		raise NotImplementedError()
 
-	@abstractmethod
 	def stop(self):
-		raise NotImplementedError()
+
+		self.__is_client_sockets_active = False
+		self.__server_socket.stop_accepting_clients()
+		for client_socket in self.__client_sockets:
+			client_socket.close()
+		self.__client_sockets.clear()
+		self.__server_socket.close()
